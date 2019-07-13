f2b2f681194eb55548d13340825eb1c594b1c743
Amaru Fernandez <amarud@gmail.com>
2019-07-13 13:04:36 -0400

    inicio

--- ProyectoX.R
+++ ProyectoX.R
@@ -3,16 +3,24 @@ library(rvest)
 paginaAntártica <-"https://www.antartica.cl/antartica/index.jsp"
 paginaANTRead<- read_html(paginaAntártica)
 nodesDelHTML<-html_nodes(paginaANTRead,".txtTitulosRutaSeccionLibros")
+
 html_attr(nodesDelHTML,"href")
 sacar<-html_attr(nodesDelHTML,"href")
 
 
 
 for (i in sacar){
-  print(i)
-  lecturalibro<-read_html(paste("https://www.antartica.cl","/antartica/servlet/LibroServlet?action=fichaLibro&id_libro=237004",sep = ""))
+  url <- paste("https://www.antartica.cl",i,sep = "")
+  lecturalibro<-read_html(url)
   titulo<- html_text(html_nodes(lecturalibro,".txtTitulosRutaSeccionLibros"))
+  titulo<- gsub("\r","",titulo)
+  titulo<- gsub("\n","",titulo)
+  precio<- html_text(html_nodes(lecturalibro,".precioAhoraFicha"))
+  internet<-html_text(html_nodes(lecturalibro,".precioAntes"))
+  splitespacio<- strsplit(internet," ")[[1]]
+  precio<-splitespacio
   print (titulo)
+  print(precio)
 }
 
 #extraccuin del contenido en la clase thumb under
