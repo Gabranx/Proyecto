8e7b92f07acd899988d67203091eba79786e62cf
Gabranx <48859270+Gabranx@users.noreply.github.com>
2019-07-19 12:34:14 -0400

    Add files via upload
    Graficos

--- ProyectoX.R
+++ ProyectoX.R
@@ -1,25 +1,17 @@
 rm(list= ls())
 library(rvest)
+
 paginaAntártica <-"https://www.antartica.cl/antartica/index.jsp"
 paginaANTRead<- read_html(paginaAntártica)
 nodesDelHTML<-html_nodes(paginaANTRead,".txtTitulosRutaSeccionLibros")
 sacar<-html_attr(nodesDelHTML,"href")
 
 tablaIndexAntartica<-html_nodes(paginaANTRead,"table")
-tablaIndexAntarticaSub1 <- html_nodes(tablaIndexAntartica,".txtBtoneraNov")
 linksTablaIz <- html_attr(tablaIndexAntarticaSub1,"href")
-tablaTmp <- html_table(tablaIndexAntarticaSub1, fill = TRUE)#Arroja error 
-variable<- 
-comicsLibro<-data.frame()
-for (x in linksTablaIz){
-  if(variable =! "/antartica/ayuda/privacidad.jsp" && variable =! "/antartica/ayuda/privacidad.jsp")
-  link <- paste("https://www.antartica.cl",x,sep = "")
-  lecturabook <-read_html(link)
-  Titulo<- html_text(html_nodes(lecturabook,".txtTitulosRutaSeccionLibros"))
-  print(Titulo)
-}
+tablaTmp <- html_table(tablaIndexAntartica, fill = TRUE)
 
-if(variable =! "/antartica/ayuda/privacidad.jsp" && variable =! "/antartica/ayuda/privacidad.jsp")
+  
+
 
 todosLibros<-data.frame()
 for (i in sacar){
@@ -45,8 +37,19 @@ for (i in sacar){
   print(normal)
   libro<-data.frame(precioNormal=normal,precioInternet=precio,nombreLibro=tituloLibro,autor=autoresLibro,editorialLibro=editorial)
   todosLibros<-rbind(libro,todosLibros)
-  plot(x = normal, y = precio) # Gráfico de dispersión
-  hist(normal) # Histograma
+  write.csv2(todosLibros, file="Tabla de precios y libros.csv")
 }
-
-#extraccuin del contenido en la clase thumb under
+#Se extraen los datos de la primera y segunda columna para luego ser comparados porcentualmente
+normalprice<- todosLibros[ , 1]
+internetprice<- todosLibros[ , 2]
+#Se crea un for con el fin de obtener la variacion porcentual entre los precios en tienda vs los precios de internet
+for (m in 1:length(normalprice)){
+  x<- (1-(internetprice/normalprice))*100
+  z<- summary(x)
+}
+datos.maxmin.LibrosVendidos<-z
+print(datos.maxmin.LibrosVendidos)
+#Se crea un gráfico de barra para observar el comportamiento de los precios en tienda
+graficopreciotienda<-hist( x= normalprice,main = "Histograma de precios",xlab = "Precios en tienda",ylab = "Frecuencia",col="blue")
+png("graficopreciotienda.png")
+graficoprecioonline<-hist(x= internetprice,main = "Histograma de precios",xlab = "Precios Online",ylab= "Frecuencia", col = "green")
